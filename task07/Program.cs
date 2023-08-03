63787b3e0fbe92bedbfdc1b129780846da2f8244
Nikita <nikitaprotska@gmail.com>
2023-08-03 13:46:49 +0300

    Написана седьмая задача

---
+++ task07/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+456 -> 6
+782 -> 2
+918 -> 8
+
+*/
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number>99 && number<1000)
+{
+    Console.WriteLine(number%10);
+}
+else
+{
+    Console.Write("Число не трехзначное");
+}
