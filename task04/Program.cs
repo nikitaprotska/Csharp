1b8cc56bef58d6f7b25b534a0d09a8d0795aee81
Nikita <nikitaprotska@gmail.com>
2023-08-03 12:53:29 +0300

    Написана четвертая задача

--- task04/Program.cs
+++ task04/Program.cs
@@ -12,7 +12,7 @@ int number2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число №3: ");
 int number3 = Convert.ToInt32(Console.ReadLine());
 int max = number1;
-if (number1>max) max=number1;
+if (number1>max)max=number1;
 if (number2>max)max=number2;
 if (number3>max)max=number3;
 Console.Write("max =");
