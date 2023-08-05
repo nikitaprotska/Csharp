657a9af222c430f0df31af30d28863d33e3a9e3d
Nikita <nikitaprotska@gmail.com>
2023-08-06 01:14:36 +0300

    Добавлена 13 задача

--- task13/Program.cs
+++ task13/Program.cs
@@ -11,7 +11,7 @@ Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 if(number<100)
 {
-    Console.WriteLine("Третьей цыфры нет");
+    Console.WriteLine("Третьей цифры нет");
 }
 else
 {
