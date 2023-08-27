4fbc2184721b3182a417a2e8d64d90694c1b2673
Nikita <nikitaprotska@gmail.com>
2023-08-28 02:56:41 +0300

    Добавлена 27 задача

--- task27/Program.cs
+++ task27/Program.cs
@@ -4,3 +4,12 @@
 82 -> 10
 9012 -> 12
 */
+
+Console.WriteLine("Введите число: ");
+int usernumber = Convert.ToInt32(Console.ReadLine());
+int num = 0;
+for (int i = usernumber; i > 0; i /= 10)
+{
+    num = i % 10 + num;    
+}
+Console.WriteLine("Сумма цифр в числе равна: " + num);
